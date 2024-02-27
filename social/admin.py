65047522f3f879e44910149a6ade7ca10a5f19be
saeed90webdev <saeed90.webdeveloper@gmail.com>
2024-02-27 12:36:02 +0330

    add CustomUser to admin

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from  django.contrib.auth.admin import UserAdmin
+from .models import *
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ["username", "first_name", "last_name", "phone" ]
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Information", {"fields": ('date_of_birth', 'bio', 'photo', 'job', 'phone')}),
+    )
+
