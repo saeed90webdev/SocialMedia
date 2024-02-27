b85ba3bab67efb649f3ce689ad9fa77c5d6b9ebd
saeed90webdev <saeed90.webdeveloper@gmail.com>
2024-02-27 11:58:47 +0330

    create CustomUser

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    "social",
+    "social", 
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CustomUser
+
+AUTH_USER_MODEL = "social.CustomUser"
